f4e267841534fe8191be0b8d3dda7788776c03a3
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-11-24 19:16:26 +0100

    Add demo connection

--- index.ts
+++ index.ts
@@ -15,7 +15,9 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  socket.on("hello", (msg) => {
+    console.log("message: " + msg);
+  });
 });
 
 server.listen(3000, () => {
