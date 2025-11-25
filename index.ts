247ad4fa7ac666afc3c01177ad88e9ecb89725a9
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-11-25 10:13:52 +0100

    Add state

--- index.ts
+++ index.ts
@@ -10,12 +10,29 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const state = createArray(200, 200);
+
+function createArray(length: number): any[];
+function createArray(length: number, arg1: number, ...rest: number[]): any[];
+function createArray(length: any): any[] {
+  var arr = new Array(length || 0),
+    i = length;
+
+  if (arguments.length > 1) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    while (i--) arr[length - 1 - i] = createArray.apply(this, args);
+  }
+
+  return arr;
+}
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
   socket.on("draw", (data) => {
+    socket.emit("message", "this is a test");
     io.emit("draw", data);
   });
 });
